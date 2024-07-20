980efe83dd43827fe5998d9081819d9dc74d09e4
VikrantKhedkar <vikrantkhedkar@outlook.com>
2024-07-21 01:02:00 +0530

    fix

--- config.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-import os
-
-client = MongoClient(os.getenv("MONGODB_ATLAS_CLUSTER_URI"))
-db = client[os.getenv("MONGODB_ATLAS_DATABASE_NAME")]  # Ensure "MONGODB_ATLAS_DATABASE_NAME" is a string
-collection = db[os.getenv("MONGODB_ATLAS_COLLECTION_NAME")]
